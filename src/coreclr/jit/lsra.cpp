5512d5d49257e2916a9a3e9697066f89edbf1f07
Mike Danes <onemihaid@hotmail.com>
2022-02-18 18:32:22 +0200

    Delete dead code

--- src/coreclr/jit/lsra.cpp
+++ src/coreclr/jit/lsra.cpp
@@ -5853,18 +5853,10 @@ void LinearScan::resolveLocalRef(BasicBlock* block, GenTreeLclVar* treeNode, Ref
             assert(currentRefPosition->refType == RefTypeExpUse);
         }
     }
-    else if (spillAfter && !RefTypeIsUse(currentRefPosition->refType) && (treeNode != nullptr) &&
-             (!treeNode->IsMultiReg() || treeNode->gtGetOp1()->IsMultiRegNode()
-#ifdef FEATURE_MULTIREG_RET
-              || treeNode->GetOp(0)->IsCopyOrReload()
-#endif
-                  ))
+    else if (spillAfter && !RefTypeIsUse(currentRefPosition->refType) && (treeNode != nullptr))
     {
         // In the case of a pure def, don't bother spilling - just assign it to the
         // stack.  However, we need to remember that it was spilled.
-        // We can't do this in the case of a multi-reg node with a non-multireg source as
-        // we need the register to extract into.
-
         assert(interval->isSpilled);
         varDsc->SetRegNum(REG_STK);
         interval->physReg = REG_NA;
