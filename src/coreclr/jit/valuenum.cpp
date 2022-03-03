26731e729e09cd48ba4996979861c89961d2eb93
Mike Danes <onemihaid@hotmail.com>
2022-03-03 22:36:04 +0200

    Remove bogus code

--- src/coreclr/jit/valuenum.cpp
+++ src/coreclr/jit/valuenum.cpp
@@ -4411,12 +4411,6 @@ void Compiler::vnIndirStore(GenTreeIndir* store, GenTreeOp* asg, GenTree* value)
 
     valueVNP = vnStore->VNPNormalPair(valueVNP);
 
-    // TODO-MIKE: This is also dubious. What exception set is this trying to obtain from a normal VN?!?
-    ValueNum valueExcVN;
-    ValueNum valueNormalVN;
-    vnStore->VNUnpackExc(valueVNP.GetLiberal(), &valueNormalVN, &valueExcVN);
-    valueVNP = ValueNumPair(valueNormalVN, vnStore->VNNormalValue(valueVNP.GetConservative()));
-
     if ((value->GetType() != store->GetType()) && value->TypeIs(TYP_REF))
     {
         // If we have an unsafe IL assignment of a TYP_REF to a non-ref (typically a TYP_BYREF)
@@ -4572,21 +4566,6 @@ void Compiler::vnLocalStore(GenTreeLclVarCommon* store, GenTreeOp* asg, GenTree*
     assert(store->OperIs(GT_LCL_VAR, GT_LCL_FLD) && ((store->gtFlags & GTF_VAR_DEF) != 0));
     assert(!GetMemorySsaMap(GcHeap)->Lookup(asg));
 
-    ValueNumPair valueVNP = value->GetVNP();
-
-    if (value->GetType() != store->GetType())
-    {
-        // TODO-MIKE: This is dubious. In general both sides of the assignment have the same type, modulo small int.
-        // While we could narrow the value here it's not clear if there's a good reason to do it because we may also
-        // need to do it when loading. And using the signedness of the value's type doesn't make a lot of sense since
-        // for small int type the value is really INT and the signedness does not matter.
-        // There are special cases like REF/BYREF and BYREF/I_IMPL conversions but it's not clear if using a VNF_Cast
-        // for those makes sense, VNF_BitCast might be preferrable. Besides, the REF/BYREF is also handled below by
-        // replacing the value VN with a new, unique one. So why bother casting to begin with?
-        bool fromUnsigned = varTypeIsUnsigned(value->GetType());
-        valueVNP          = vnStore->VNPairForCast(valueVNP, store->GetType(), value->GetType(), fromUnsigned);
-    }
-
     LclVarDsc* lcl = lvaGetDesc(store);
 
     if (lcl->IsAddressExposed())
@@ -4603,13 +4582,22 @@ void Compiler::vnLocalStore(GenTreeLclVarCommon* store, GenTreeOp* asg, GenTree*
         return;
     }
 
-    valueVNP = vnStore->VNPNormalPair(valueVNP);
+    ValueNumPair valueVNP = value->GetVNP();
 
-    // TODO-MIKE: This is also dubious. What exception set is this trying to obtain from a normal VN?!?
-    ValueNum valueExcVN;
-    ValueNum valueNormalVN;
-    vnStore->VNUnpackExc(valueVNP.GetLiberal(), &valueNormalVN, &valueExcVN);
-    valueVNP = ValueNumPair(valueNormalVN, vnStore->VNNormalValue(valueVNP.GetConservative()));
+    if (value->GetType() != store->GetType())
+    {
+        // TODO-MIKE: This is dubious. In general both sides of the assignment have the same type, modulo small int.
+        // While we could narrow the value here it's not clear if there's a good reason to do it because we may also
+        // need to do it when loading. And using the signedness of the value's type doesn't make a lot of sense since
+        // for small int type the value is really INT and the signedness does not matter.
+        // There are special cases like REF/BYREF and BYREF/I_IMPL conversions but it's not clear if using a VNF_Cast
+        // for those makes sense, VNF_BitCast might be preferrable. Besides, the REF/BYREF is also handled below by
+        // replacing the value VN with a new, unique one. So why bother casting to begin with?
+        bool fromUnsigned = varTypeIsUnsigned(value->GetType());
+        valueVNP          = vnStore->VNPairForCast(valueVNP, store->GetType(), value->GetType(), fromUnsigned);
+    }
+
+    valueVNP = vnStore->VNPNormalPair(valueVNP);
 
     if ((value->GetType() != store->GetType()) && value->TypeIs(TYP_REF))
     {
