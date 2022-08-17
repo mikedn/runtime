daebc6e41d75131b8428cdb9ec51d9ef46044cf8
Mike Danes <onemihaid@hotmail.com>
2022-08-17 22:48:21 +0300

    Move misplaced comment

--- src/coreclr/jit/fgbasic.cpp
+++ src/coreclr/jit/fgbasic.cpp
@@ -795,6 +795,29 @@ private:
     unsigned depth;
 };
 
+//------------------------------------------------------------------------
+// fgFindJumpTargets: walk the IL stream, determining jump target offsets
+//
+// Arguments:
+//    codeAddr   - base address of the IL code buffer
+//    codeSize   - number of bytes in the IL code buffer
+//    jumpTarget - [OUT] bit vector for flagging jump targets
+//
+// Notes:
+//    If inlining or prejitting the root, this method also makes
+//    various observations about the method that factor into inline
+//    decisions.
+//
+//    May throw an exception if the IL is malformed.
+//
+//    jumpTarget[N] is set to 1 if IL offset N is a jump target in the method.
+//
+//    Also sets lvAddrExposed and lvHasILStoreOp, ilHasMultipleILStoreOp in lvaTable[].
+
+#ifdef _PREFAST_
+#pragma warning(push)
+#pragma warning(disable : 21000) // Suppress PREFast warning about overly large function
+#endif
 void Compiler::fgFindJumpTargets(const BYTE* codeAddr, IL_OFFSET codeSize, FixedBitVect* jumpTarget)
 {
     const BYTE* codeBegp = codeAddr;
@@ -2078,6 +2101,9 @@ void Compiler::fgFindJumpTargets(const BYTE* codeAddr, IL_OFFSET codeSize, Fixed
         }
     }
 }
+#ifdef _PREFAST_
+#pragma warning(pop)
+#endif
 
 //------------------------------------------------------------------------
 // fgAdjustForAddressExposedOrWrittenThis: update var table for cases
